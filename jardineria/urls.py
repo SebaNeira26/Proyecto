70f33b0cd09f7ebca31cb315c7aa5ea400f56fa4
SebaNeira26 <you@example.com>
2024-07-15 18:16:05 -0400

    2

--- jardineria/urls.py
+++ jardineria/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
-    path('productos', views.productos, name='productos'),  
+    path('formulario', views.index, name='formulario'),
+    path('productos', views.productos, name='productos'), 
+    path('formulario', views.formulario, name='formulario'), 
 ]
--- jardineria/views.py
+++ jardineria/views.py
@@ -12,4 +12,8 @@ def productos(request):
     productos = Producto.objects.all()
     return render(request, 'jardineria/productos.html', {'productos': productos})
 
+def formulario(request):
+    context={}
+    return render(request, 'jardineria/formulario.html', context)
+
 
