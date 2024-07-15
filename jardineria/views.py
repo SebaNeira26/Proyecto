62a92e039a721d34133740503b44117bdd586cee
SebaNeira26 <you@example.com>
2024-07-15 16:41:22 -0400

    first commit

---
+++ jardineria/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Producto
+
+# Create your views here.
+
+
+def index(request):
+    context={}
+    return render(request, 'jardineria/index.html', context)
+
+def productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'jardineria/productos.html', {'productos': productos})
+
+
